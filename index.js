50d1fcdbd57dd9ef747200218fd9fc15a91f79f2
PattersonOfficial <akrongwork@gmail.com>
2021-08-19 14:34:15 +0000

    business card package complete

---
+++ index.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node 
+
+console.log(`
+    ***************************************************************************
+    * Name: Benjamin Akrong               Profile 👇
+    ***************************************************************************
+    * Bio: Christian, Software Engineer, Music Lover, Creatives etc
+    ***************************************************************************
+    * LinkedIn: https://www.linkedin.com/in/benjamin-akrong-251025132/
+    * Github: https://github.com/PattersonOfficial
+    * Twitter: https://twitter.com/ace_fist
+    * Facebook: https://web.facebook.com/ace.fist.14/
+    * Hashnode: https://hashnode.com/@AkrongOfficial
+    * StackOverflow: https://stackoverflow.com/users/10206785/benjamin-akrong
+    ***************************************************************************
+    * Major Accomplishment 
+    ***************************************************************************
+    * Asher Business Suite: https://www.ashersuite.com/
+    * NubiaEMR: https://nubiaemr.com/
+    * MeroeHP:  http://www.meroehp.com/home
+    * SronuSMS: https://sronuschoolmanager.com/
+    ***************************************************************************
+`);
